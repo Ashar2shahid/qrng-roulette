b60ec188f2c49e6b2d7c463be0d99b9989747ac9
beejsbj <burooj.bj@gmail.com>
2023-03-22 16:21:53 -0400

    IsConnected for roulette

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,19 @@ import TitleSign from "./components/TitleSign";
 import ConnectButton from "./components/ConnectButton";
 import Roulette from "./components/Roulette";
 
+import { useAccount } from "wagmi";
+
 function App() {
+  const { isConnected } = useAccount();
+
   return (
     <main className="App page-content">
       <section className="page-section">
         <ConnectButton />
         <inner-column>
           <TitleSign />
-          <Roulette />
+          {isConnected && <Roulette />}
+          {!isConnected && <Roulette />}
         </inner-column>
       </section>
     </main>
