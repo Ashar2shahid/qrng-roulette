2cc2b35c2c0c6a42b88c55bec3cbae8ad8531dec
beejsbj <burooj.bj@gmail.com>
2023-03-22 13:40:38 -0400

    Initial organization

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-	port: 7173,
- }
-})
+    port: 7174,
+  },
+  preview: {
+    port: 7174,
+  },
+});
